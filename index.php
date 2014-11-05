706b39b0a6a606545f8747701633c30154681301
marcd <marcd@leadqual.com>
2014-11-04 16:02:10 -0800

    added another print

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php print("Hi"); print("<br>Today's date is: " . date('m-d-Y H:i:s') . "<br>");
+<?php 
+	print("Hi"); 
+	print("<br>Today's date is: " . date('m-d-Y H:i:s') . "<br>");
+	print("This is being stored in my git account and on the git created by heroku");
