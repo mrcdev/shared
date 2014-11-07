93682fc4914d809197a60a517c7e7625dea2dd45
marcd <marcd@leadqual.com>
2014-11-06 16:21:12 -0800

    nothing

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 	print("Hi"); 
 	print("<br>Today's date is: " . date('m-d-Y H:i:s') . "<br>");
 	print("This is being stored in my git account and on the git created by heroku");
-	print("Heroku change");
+	print("Git change");
